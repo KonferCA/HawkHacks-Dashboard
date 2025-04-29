e2a37cfc0425e04e8535b1d73af62005f5ec9e9a
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-04-28 20:32:55 -0400

    [update] infoCallout component to use chakra alert

--- src/components/InfoCallout/InfoCallout.tsx
+++ src/components/InfoCallout/InfoCallout.tsx
@@ -3,19 +3,19 @@ import { InformationCircleIcon } from "@heroicons/react/20/solid";
 import type { FC } from "react";
 
 interface InfoCalloutProps {
-    text: string;
+	text: string;
 }
 
 export const InfoCallout: FC<InfoCalloutProps> = ({ text }) => {
-    return (
-        <Alert.Root status="info" bg="blue.50" p={4}>
-            <Alert.Indicator>
-                <InformationCircleIcon
-                    className="h-5 w-5 text-blue-400"
-                    aria-hidden="true"
-                />
-            </Alert.Indicator>
-            <Alert.Title color="blue.600">{text}</Alert.Title>
-        </Alert.Root>
-    );
+	return (
+		<Alert.Root status="info" bg="blue.50" p={4}>
+			<Alert.Indicator>
+				<InformationCircleIcon
+					className="h-5 w-5 text-blue-400"
+					aria-hidden="true"
+				/>
+			</Alert.Indicator>
+			<Alert.Title color="blue.600">{text}</Alert.Title>
+		</Alert.Root>
+	);
 };
