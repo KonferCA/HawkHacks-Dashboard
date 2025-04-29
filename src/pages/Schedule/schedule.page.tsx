a730f809c82a2c492fab60bf9dcc6eab5202386b
Benjamin Sengupta <benjaminsengupta@gmail.com>
2025-04-29 19:02:00 -0400

    feat: use date objects in schedule
    Simplify grid increment

--- src/pages/Schedule/schedule.page.tsx
+++ src/pages/Schedule/schedule.page.tsx
@@ -6,7 +6,7 @@ import {
 	ScheduleTabTrigger,
 	ScheduleGrid,
 	ScheduleGridItem,
-	ScheduleGridItemProps,
+	type ScheduleGridItemProps,
 	format12HourTime,
 } from "@/components/Schedule/Schedule";
 
@@ -27,96 +27,125 @@ function ScheduleEntry(props: ScheduleEntryProps) {
 	);
 }
 
+const schedule = [
+	{
+		startDate: new Date("2025-07-25T09:30:00"),
+		endTime: new Date("2025-07-25T11:30:00"),
+		color: "teal",
+		title: "Registration",
+		location: "Location",
+	},
+	{
+		startDate: new Date("2025-07-25T11:30:00"),
+		endTime: new Date("2025-07-25T12:00:00"),
+		color: "green",
+		title: "Robotics Workshop",
+		location: "Location",
+	},
+	{
+		startDate: new Date("2025-07-25T19:00:00"),
+		endTime: new Date("2025-07-25T20:30:00"),
+		color: "purple",
+		title: "Musical Chairs",
+		location: "Location",
+	},
+	{
+		startDate: new Date("2025-07-25T11:00:00"),
+		endTime: new Date("2025-07-25T12:30:00"),
+		color: "purple",
+		title: "Zumba",
+		location: "Location",
+	},
+	{
+		startDate: new Date("2025-07-25T14:00:00"),
+		endTime: new Date("2025-07-25T15:00:00"),
+		color: "red",
+		title: "Sponsor",
+		location: "Location",
+	},
+	{
+		startDate: new Date("2025-07-25T11:00:00"),
+		endTime: new Date("2025-07-25T14:00:00"),
+		color: "red",
+		title: "Networking",
+		location: "Location",
+	},
+	{
+		startDate: new Date("2025-07-25T14:00:00"),
+		endTime: new Date("2025-07-25T15:30:00"),
+		color: "purple",
+		title: "Pictureka",
+		location: "Location",
+	},
+	{
+		startDate: new Date("2025-07-26T09:30:00"),
+		endTime: new Date("2025-07-26T11:30:00"),
+		color: "teal",
+		title: "Registration",
+		location: "Location",
+	},
+	{
+		startDate: new Date("2025-07-27T09:30:00"),
+		endTime: new Date("2025-07-27T11:30:00"),
+		color: "teal",
+		title: "Registration",
+		location: "Location",
+	},
+];
+
 export const SchedulePage: React.FC = () => {
+	const groupedSchedule = schedule
+		.map((event) => ({
+			title: event.title,
+			location: event.location,
+			startTime:
+				`${event.startDate.getHours()}:${event.startDate.getMinutes()}` as const,
+			endTime:
+				`${event.endTime.getHours()}:${event.endTime.getMinutes()}` as const,
+			color: event.color,
+			day: event.startDate.toLocaleDateString("en-US", {
+				day: "numeric",
+				month: "long",
+				weekday: "long",
+			}),
+		}))
+		.reduce((acc, entry) => {
+			const dayArray = acc.get(entry.day) ?? [];
+			acc.set(entry.day, dayArray);
+			dayArray.push(entry);
+			return acc;
+		}, new Map<string, ScheduleEntryProps[]>());
+
+	const scheduleEntries = Array.from(groupedSchedule.entries());
+
 	return (
 		<PageWrapper>
-			<ScheduleRoot defaultValue="day1">
+			<ScheduleRoot defaultValue={scheduleEntries[0][0]}>
 				<ScheduleTabList>
-					<ScheduleTabTrigger value="day1">Day 1</ScheduleTabTrigger>
-					<ScheduleTabTrigger value="day2">Day 2</ScheduleTabTrigger>
-					<ScheduleTabTrigger value="day3">Day 3</ScheduleTabTrigger>
+					{scheduleEntries.map(([day]) => (
+						<ScheduleTabTrigger key={day} value={day}>
+							{day}
+						</ScheduleTabTrigger>
+					))}
 				</ScheduleTabList>
 
-				<ScheduleTabContent value="day1">
-					<ScheduleGrid>
-						<ScheduleEntry
-							scrollIntoViewOnMount
-							startTime="9:30"
-							endTime="11:30"
-							color="teal"
-							title="Registration"
-							location="Location"
-						/>
-						<ScheduleEntry
-							startTime="11:30"
-							endTime="12:00"
-							color="green"
-							title="Robotics Workshop"
-							location="Location"
-						/>
-						<ScheduleEntry
-							startTime="19:00"
-							endTime="20:30"
-							color="purple"
-							title="Musical Chairs"
-							location="Location"
-						/>
-						<ScheduleEntry
-							startTime="11:00"
-							endTime="12:30"
-							color="purple"
-							title="Zumba"
-							location="Location"
-						/>
-						<ScheduleEntry
-							startTime="14:00"
-							endTime="15:00"
-							color="red"
-							title="Sponsor"
-							location="Location"
-						/>
-						<ScheduleEntry
-							startTime="11:00"
-							endTime="14:00"
-							color="red"
-							title="Networking"
-							location="Location"
-						/>
-						<ScheduleEntry
-							startTime="14:00"
-							endTime="15:30"
-							color="purple"
-							title="Pictureka"
-							location="Location"
-						/>
-					</ScheduleGrid>
-				</ScheduleTabContent>
-
-				<ScheduleTabContent value="day2">
-					<ScheduleGrid>
-						<ScheduleEntry
-							scrollIntoViewOnMount
-							startTime="9:30"
-							endTime="11:30"
-							color="teal"
-							title="Registration"
-							location="Location"
-						/>
-					</ScheduleGrid>
-				</ScheduleTabContent>
-
-				<ScheduleTabContent value="day3">
-					<ScheduleGrid>
-						<ScheduleEntry
-							scrollIntoViewOnMount
-							startTime="9:30"
-							endTime="11:30"
-							color="teal"
-							title="Registration"
-							location="Location"
-						/>
-					</ScheduleGrid>
-				</ScheduleTabContent>
+				{scheduleEntries.map(([day, entries]) => (
+					<ScheduleTabContent key={day} value={day}>
+						<ScheduleGrid>
+							{entries.map((entry, idx) => (
+								<ScheduleEntry
+									key={entry.title}
+									scrollIntoViewOnMount={idx === 0}
+									startTime={entry.startTime}
+									endTime={entry.endTime}
+									color={entry.color}
+									title={entry.title}
+									location={entry.location}
+								/>
+							))}
+						</ScheduleGrid>
+					</ScheduleTabContent>
+				))}
 			</ScheduleRoot>
 		</PageWrapper>
 	);
