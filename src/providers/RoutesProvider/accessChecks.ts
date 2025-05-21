d7ad54c3cc7dc4ffa0c7591ddfc22cafd55b6648
Benjamin Roman Perez <59741853+roman20203@users.noreply.github.com>
2025-05-21 17:33:38 -0400

    Biome fix formatting for accessChecks.ts

--- src/providers/RoutesProvider/accessChecks.ts
+++ src/providers/RoutesProvider/accessChecks.ts
@@ -26,19 +26,19 @@ export const isHacker: AccessControlFn = ({ user }) => user?.type === "hacker";
  * Checks if user is a Tier 1 Volunteer
  */
 export const isVolunteerT1: AccessControlFn = ({ user }) =>
-    user?.type === "volunteer.t1";
+	user?.type === "volunteer.t1";
 
 /**
  * Checks if user is a Tier 2 Volunteer
  */
 export const isVolunteerT2: AccessControlFn = ({ user }) =>
-    user?.type === "volunteer.t2";
+	user?.type === "volunteer.t2";
 
 /**
  * Checks if user is a sponsor
  */
 export const isSponsor: AccessControlFn = ({ user }) =>
-    user?.type === "sponsor";
+	user?.type === "sponsor";
 
 /**
  * Checks if user is a mentor
@@ -54,35 +54,43 @@ export const isVIP: AccessControlFn = ({ user }) => user?.type === "vip";
  * Checks if user is a speaker
  */
 export const isSpeaker: AccessControlFn = ({ user }) =>
-    user?.type === "speaker";
+	user?.type === "speaker";
 
 /**
  * Checks if user has RSVP verified
  */
 export const isRSVPVerified: AccessControlFn = ({ user }) =>
-    !!user?.rsvpVerified;
+	!!user?.rsvpVerified;
 
 /**
  * Checks if user's application is accepted
  */
 export const applicationAccepted: AccessControlFn = () => {
-    const { mostRecentApplication } = useApplications();
-    return mostRecentApplication?.applicationStatus === "accepted" && mostRecentApplication?.accepted === true;
+	const { mostRecentApplication } = useApplications();
+	return (
+		mostRecentApplication?.applicationStatus === "accepted" &&
+		mostRecentApplication?.accepted === true
+	);
 };
 
 /**
  * Checks if user's application is rejected
  */
 export const applicationRejected: AccessControlFn = () => {
-    const { mostRecentApplication } = useApplications();
-    return mostRecentApplication?.applicationStatus === "rejected" && mostRecentApplication?.accepted === false;
+	const { mostRecentApplication } = useApplications();
+	return (
+		mostRecentApplication?.applicationStatus === "rejected" &&
+		mostRecentApplication?.accepted === false
+	);
 };
 
-/** 
+/**
  * Checks if application is pending
  */
 export const applicationPending: AccessControlFn = () => {
-    const { mostRecentApplication } = useApplications();
-    return mostRecentApplication?.applicationStatus === "pending" && mostRecentApplication?.accepted === false;
+	const { mostRecentApplication } = useApplications();
+	return (
+		mostRecentApplication?.applicationStatus === "pending" &&
+		mostRecentApplication?.accepted === false
+	);
 };
-
